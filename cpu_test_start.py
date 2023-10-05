8ebff36687845f4092cfaec10d1f264060ccd5d7
Stepan Vladovskiy <duffokin@gmail.com>
2023-10-04 21:58:49 -0300

    feat: status bar

--- cpu_test_start.py
+++ cpu_test_start.py
@@ -1,22 +1,31 @@
 import psycopg2
 import argparse
 from random import randint
+from tqdm import tqdm  # <-- New import for the status bar
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description='Generate CPU load on a PostgreSQL database.')
 parser.add_argument('-dsn', '--data_source_name', type=str, required=True, help='The Data Source Name (DSN) for connecting to PostgreSQL.')
 args = parser.parse_args()
 
-# Connect to PostgreSQL database using the provided DSN
-conn = psycopg2.connect(args.data_source_name)
+print("Attempting to connect to the database...")
+try:
+    # Connect to PostgreSQL database using the provided DSN
+    conn = psycopg2.connect(args.data_source_name)
+    print("Successfully connected to the database.")
+except Exception as e:
+    print(f"Failed to connect to the database: {e}")
+    exit(1)
 
 # Create a table
 cur = conn.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS test_table (id serial PRIMARY KEY, num integer);")
 conn.commit()
+print("Table 'test_table' created or already exists.")
 
 # Insert random data into the table in a loop to generate load
-while True:
+print("Starting to insert data and run queries...")
+for i in tqdm(range(1000)):  # <-- Using tqdm for the status bar, running 1000 iterations
     random_num = randint(0, 1000)
     cur.execute("INSERT INTO test_table (num) VALUES (%s);", (random_num,))
     conn.commit()
@@ -25,6 +34,9 @@ while True:
     cur.execute("SELECT num, COUNT(*) FROM test_table GROUP BY num ORDER BY COUNT(*) DESC;")
     rows = cur.fetchall()
 
-# Close the cursor and the connection (though this part won't be reached in an infinite loop)
+print("Completed 1000 iterations.")
+
+# Close the cursor and the connection
 cur.close()
 conn.close()
+print("Cursor and connection closed.")
