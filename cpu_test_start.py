aaeefbeb83b18dfc5f4eb76831238a5acba0f57b
Stepan Vladovskiy <duffokin@gmail.com>
2023-10-04 22:17:37 -0300

    feat: few instance

--- cpu_test_start.py
+++ cpu_test_start.py
@@ -1,42 +1,54 @@
 import psycopg2
 import argparse
+import threading
 from random import randint
-from tqdm import tqdm  # <-- New import for the status bar
+from tqdm import tqdm
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description='Generate CPU load on a PostgreSQL database.')
-parser.add_argument('-dsn', '--data_source_name', type=str, required=True, help='The Data Source Name (DSN) for connecting to PostgreSQL.')
-args = parser.parse_args()
+def run_queries(dsn):
+    try:
+        conn = psycopg2.connect(dsn)
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS test_table (id serial PRIMARY KEY, num integer);")
+        cur.execute("CREATE TABLE IF NOT EXISTS another_table (id serial PRIMARY KEY, value integer);")
+        conn.commit()
 
-print("Attempting to connect to the database...")
-try:
-    # Connect to PostgreSQL database using the provided DSN
-    conn = psycopg2.connect(args.data_source_name)
-    print("Successfully connected to the database.")
-except Exception as e:
-    print(f"Failed to connect to the database: {e}")
-    exit(1)
+        for i in tqdm(range(1000)):
+            random_num = randint(0, 1000)
+            cur.execute("INSERT INTO test_table (num) VALUES (%s);", (random_num,))
+            cur.execute("INSERT INTO another_table (value) VALUES (%s);", (random_num,))
+            conn.commit()
 
-# Create a table
-cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS test_table (id serial PRIMARY KEY, num integer);")
-conn.commit()
-print("Table 'test_table' created or already exists.")
+            cur.execute("""
+            SELECT t1.num, t2.value, COUNT(*)
+            FROM test_table t1
+            JOIN another_table t2 ON t1.id = t2.id
+            GROUP BY t1.num, t2.value
+            ORDER BY COUNT(*) DESC;
+            """)
+            rows = cur.fetchall()
+        
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print(f"Thread failed: {e}")
 
-# Insert random data into the table in a loop to generate load
-print("Starting to insert data and run queries...")
-for i in tqdm(range(1000)):  # <-- Using tqdm for the status bar, running 1000 iterations
-    random_num = randint(0, 1000)
-    cur.execute("INSERT INTO test_table (num) VALUES (%s);", (random_num,))
-    conn.commit()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Generate CPU load on a PostgreSQL database.')
+    parser.add_argument('-dsn', '--data_source_name', type=str, required=True, help='The Data Source Name (DSN) for connecting to PostgreSQL.')
+    args = parser.parse_args()
 
-    # Run a CPU-intensive query
-    cur.execute("SELECT num, COUNT(*) FROM test_table GROUP BY num ORDER BY COUNT(*) DESC;")
-    rows = cur.fetchall()
+    num_threads = 5  # Number of threads to create
 
-print("Completed 1000 iterations.")
+    print(f"Starting {num_threads} threads to connect to the database and run queries.")
 
-# Close the cursor and the connection
-cur.close()
-conn.close()
-print("Cursor and connection closed.")
+    threads = []
+    for i in range(num_threads):
+        thread = threading.Thread(target=run_queries, args=(args.data_source_name,))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+
+    print("All threads completed.")
+
